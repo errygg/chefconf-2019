9d33b441f6d44db4808bfa95d5f79d17fa87f10f
Erik R. Rygg <errygg@gmail.com>
2019-05-13 21:11:12 -0600

    Working demo!

--- services/dashboard/habitat/plan.sh
+++ services/dashboard/habitat/plan.sh
@@ -2,7 +2,7 @@ pkg_origin=errygg
 pkg_name=dashboard
 pkg_version=0.1.0
 pkg_description="Example dashboard application"
-pkg_deps=( core/bash core/curl core/go )
+pkg_deps=( core/curl core/bind core/envoy core/go errygg/dnsmasq/2.80/20190513203448 )
 pkg_bin_dirs=(bin)
 pkg_scaffolding=core/scaffolding-go
 
@@ -11,3 +11,6 @@ scaffolding_go_build_deps=(
   github.com/graarh/golang-socketio
   github.com/GeertJohan/go.rice
 )
+
+pkg_svc_user="root"
+pkg_svc_group="root"
